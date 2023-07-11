e54876457d209fcf62f382a4e6c42ceb8b4ffdad
Shaikh Al Amin <alamin.cse15@gmail.com>
2023-07-11 17:09:11 +0600

    new feature added with admin route

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 // GET route
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.get("/new-route", (req, res) => {
+  res.send("new route added for testing");
+});
+
+app.get("/admin", (req, res) => {
+  res.send("admin route added testing");
 });
 
 // POST route
-app.post('/data', (req, res) => {
+app.post("/data", (req, res) => {
   // Handle the POST request and send a response
-  res.send('Received POST request');
+  res.send("Received POST request");
 });
 
 // Start the server
